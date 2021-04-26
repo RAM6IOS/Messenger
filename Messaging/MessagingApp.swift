b327d8ab12e6e633140fb86a1e78ff9d0f0dc3fd
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-26 17:25:39 +0100

    Initial Commit

---
+++ Messaging/MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingApp.swift
+//  Messaging
+//
+//  Created by MAC on 26/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
